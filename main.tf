53e0e7622bf012cbb897124d30437b3d8b203437
Rod Stewart <rodstewart@gmail.com>
2022-10-14 21:27:49 -0500

    Untracks root main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "baseinfrastructure" {
-  source           = "./baseinfrastructure"
-  resource_groups  = var.resource_groups
-  virtual_networks = var.virtual_networks
-  subnets          = var.subnets
-  nsgs             = var.nsgs
-  tags = var.tags
-}
